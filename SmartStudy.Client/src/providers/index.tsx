60eb8489c4b4e583fb7872ebf19b1bf87af202c7
Limito9x <limito9x@gmail.com>
2026-02-22 21:45:36 +0700

    fix config

--- SmartStudy.Client/src/providers/index.tsx
+++ SmartStudy.Client/src/providers/index.tsx
@@ -1,10 +1,15 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { UIProviders } from "./ui-provider";
 import { BrowserRouter } from "react-router-dom";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <UIProviders>{children}</UIProviders>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <UIProviders>{children}</UIProviders>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
--- SmartStudy.Client/src/routes/index.tsx
+++ SmartStudy.Client/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { type RouteObject, useRoutes } from "react-router-dom";
 import DashboardLayout from "@/layouts/DashboardLayout";
 import AuthLayout from "@/layouts/AuthLayout";
+import LoginPage from "@/pages/Auth/LoginPage";
 import SchoolStudyPage from "@/pages/SchoolStudy/SchoolStudy";
 
 export default function AppRoutes() {
@@ -21,7 +22,7 @@ export default function AppRoutes() {
         {
           path: "school",
           element: <SchoolStudyPage />,
-        }
+        },
       ],
     },
     {
@@ -30,7 +31,8 @@ export default function AppRoutes() {
       children: [
         {
           path: "login",
-          element: <div>Login Page</div>,
+          element: <LoginPage />,
+          index: true,
         },
         {
           path: "register",
