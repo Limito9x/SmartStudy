29e43ce10f2e97b07d6011e674f590aeddce320c
Limito9x <limito9x@gmail.com>
2026-02-22 21:44:15 +0700

    create login form

---
+++ SmartStudy.Client/src/pages/Auth/LoginPage.tsx
@@ -0,0 +1,78 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { authService } from "@/services/apiClient";
+import { type UserLoginDto } from "@/services/api";
+import { useAuthStore } from "@/stores/useAuthStore";
+
+export default function LoginPage() {
+  const loginFormSchema = z.object({
+    userName: z.string().min(1, "Vui lòng nhập tên đăng nhập"),
+    password: z.string().min(1, "Vui lòng nhập mật khẩu"),
+  }) satisfies z.ZodType<UserLoginDto>;
+
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      userName: "",
+      password: "",
+    },
+  });
+
+  const handleLogin = async (values: z.infer<typeof loginFormSchema>) => {
+    try {
+      const response = await authService.apiAuthLoginPost(values);
+      useAuthStore.getState().login(response.data);
+      console.log("Login successful:", response.data);
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Đăng nhập</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleLogin)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="userName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tên đăng nhập</FormLabel>
+                <FormControl>
+                  <Input placeholder="Tên đăng nhập" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mật khẩu</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Mật khẩu" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Đăng nhập</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
